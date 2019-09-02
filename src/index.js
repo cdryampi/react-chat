f9110c89e4ca25eaedbe89491da94b419e0d3802
yampi <cdryampi@gmail.com>
2019-09-02 20:27:47 +0200

    js html css vitamizado

--- src/index.js
+++ src/index.js
@@ -1,28 +1,11 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import "./custom.css";
+//import ChatRoom from './components/ChatRoom';
 
-import ChatRoom from './components/ChatRoom';
 
-class App extends Component {
-  render() {
-   return (
-    <div>
-      <nav className="navbar navbar-light bg-primary text-white">
-        <a className="navbar-brand">React Chat</a>
-      </nav>
-      <div className="container p-5">
-        <div className="row">
-          <div className="col-md-6 offset-md-3">
-            <ChatRoom/>
-          </div>
-        </div>
-      </div>
-    </div>
-   ) 
-  }
-}
 
 render(
-  <App/>,
+  <h1>Puta madre!</h1>,
   document.getElementById('app')
 );
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: './src/index.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: './dist',
+        port: 3000
+    },
+    module: {
+        rules: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'env']
+                }
+            },
+            {
+                // Test de html
+                test: /\.html$/,
+                use: [{
+                    loader: 'html-loader'
+                }]
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        })
+    ]
+};
