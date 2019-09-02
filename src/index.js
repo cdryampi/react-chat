22031abffd4dc513c3e1663feb2e8f0ed7eca6c4
yampi <cdryampi@gmail.com>
2019-09-03 00:38:01 +0200

    web personalizada 1

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import "./custom.css";
-//import ChatRoom from './components/ChatRoom';
+//import Icon from './img/llaç.png'
+// //import ChatRoom from './components/ChatRoom';
+// function icono(){
+//   const element = document.createElement('div')
+//   const myIcon = new Image()
+//   myIcon.src = Icon;
 
-
-
+//   element.innerHTML = _.join(['Hello','webpack'],'')
+//   element.classList.add('hello')
+//   myIcon.src = Icon
+//   element.appendChild(myIcon)
+//   return element
+// }
+// document.body.appendChild(icono());
 render(
-  <h1 class="text-warning">Visca Catalunya</h1>,
+
+  <h1 className="text-warning">Visca Catalunya</h1>,
   document.getElementById('app')
-);
+)
+
--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,12 @@ module.exports = {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader'],
             },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            }
         ],
     },
     plugins: [
