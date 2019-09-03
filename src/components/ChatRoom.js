cd4eec0c1b5048425dd5dc1079a979221fe97a5a
yampi <cdryampi@gmail.com>
2019-09-03 18:38:52 +0200

    antes de los bucles

--- src/components/ChatRoom.js
+++ src/components/ChatRoom.js
@@ -7,17 +7,20 @@ class ChatRoom extends Component {
       <div className="inbox_people">
           <div className="headind_srch">
               <div className="recent_heading">
-                  <h4>Recent</h4>
+                  <h4>En línea</h4>
               </div>
+              {/* Input de busqueda de usuarios por nickname */}
               <div className="srch_bar">
                   <div className="stylish-input-group">
-                      <input type="text" class="search-bar" placeholder="Search"/>
+                      <input type="text" class="search-bar" placeholder="Buscar"/>
                       <span className="input-group-addon">
-                                  <button type="button"> <i class="fa fa-search" aria-hidden="true"></i> </button>
+                                  <button type="button">
+                                     <i class="fa fa-search" aria-hidden="true"></i> 
+                                  </button>
                       </span> 
               </div>
-
               </div>
+              {/* fin del input de busqueda de usuarios */}
           </div>
           <div className="inbox_chat">
               <div className="chat_list active_chat">
@@ -29,63 +32,11 @@ class ChatRoom extends Component {
                       </div>
                   </div>
               </div>
-              <div className="chat_list">
-                  <div className="chat_people">
-                      <div className="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"/> </div>
-                      <div className="chat_ib">
-                          <h5>Sunil Rajput <span className="chat_date">Dec 25</span></h5>
-                          <p>Test, which is a new approach to have all solutions astrology under one roof.</p>
-                      </div>
-                  </div>
-              </div>
-              <div className="chat_list">
-                  <div className="chat_people">
-                      <div className="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"/> </div>
-                      <div className="chat_ib">
-                          <h5>Sunil Rajput <span className="chat_date">Dec 25</span></h5>
-                          <p>Test, which is a new approach to have all solutions astrology under one roof.</p>
-                      </div>
-                  </div>
-              </div>
-              <div className="chat_list">
-                  <div className="chat_people">
-                      <div className="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"/> </div>
-                      <div className="chat_ib">
-                          <h5>Sunil Rajput <span className="chat_date">Dec 25</span></h5>
-                          <p>Test, which is a new approach to have all solutions astrology under one roof.</p>
-                      </div>
-                  </div>
-              </div>
-              <div className="chat_list">
-                  <div className="chat_people">
-                      <div className="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"/> </div>
-                      <div className="chat_ib">
-                          <h5>Sunil Rajput <span className="chat_date">Dec 25</span></h5>
-                          <p>Test, which is a new approach to have all solutions astrology under one roof.</p>
-                      </div>
-                  </div>
-              </div>
-              <div className="chat_list">
-                  <div className="chat_people">
-                      <div className="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"/> </div>
-                      <div className="chat_ib">
-                          <h5>Sunil Rajput <span className="chat_date">Dec 25</span></h5>
-                          <p>Test, which is a new approach to have all solutions astrology under one roof.</p>
-                      </div>
-                  </div>
-              </div>
-              <div className="chat_list">
-                  <div className="chat_people">
-                      <div className="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"/> </div>
-                      <div className="chat_ib">
-                          <h5>Sunil Rajput <span className="chat_date">Dec 25</span></h5>
-                          <p>Test, which is a new approach to have all solutions astrology under one roof.</p>
-                      </div>
-                  </div>
-              </div>
+          {/* Realizar un bucle para rellenar los div con los usuarios en línea */}
           </div>
       </div>
       <div className="mesgs">
+        {/*  */}
           <div className="msg_history">
               <div className="incoming_msg">
                   <div className="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"/> </div>
@@ -102,27 +53,7 @@ class ChatRoom extends Component {
                       </p>
                       <span className="time_date"> 11:01 AM    |    June 9</span> </div>
               </div>
-              <div className="incoming_msg">
-                  <div className="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"/> </div>
-                  <div className="received_msg">
-                      <div className="received_withd_msg">
-                          <p>Test, which is a new approach to have</p>
-                          <span className="time_date"> 11:01 AM    |    Yesterday</span></div>
-                  </div>
-              </div>
-              <div className="outgoing_msg">
-                  <div className="sent_msg">
-                      <p>Apollo University, Delhi, India Test</p>
-                      <span className="time_date"> 11:01 AM    |    Today</span> </div>
-              </div>
-              <div className="incoming_msg">
-                  <div className="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"/> </div>
-                  <div className="received_msg">
-                      <div className="received_withd_msg">
-                          <p>We work directly with our designers and suppliers, and sell direct to you, which means quality, exclusive products, at a price anyone can afford.</p>
-                          <span className="time_date"> 11:01 AM    |    Today</span></div>
-                  </div>
-              </div>
+
           </div>
           <div className="type_msg">
               <div className="input_msg_write">
