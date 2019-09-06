f52f6c865a6300363a4b93726c6d8bb306a93311
yampi <cdryampi@gmail.com>
2019-09-06 22:13:43 +0200

    UID de header.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -12,7 +12,17 @@ class Login extends Component {
             signOut,
             signInWithGoogle,
         } = this.props;
+        var user2 = firebase.auth().currentUser;
 
+        if (user2 != null) {
+          user2.providerData.forEach(function (profile) {
+            console.log("Sign-in provider: " + profile.providerId);
+            console.log("  Provider-specific UID: " + profile.uid);
+            console.log("  Name: " + profile.displayName);
+            console.log("  Email: " + profile.email);
+            console.log("  Photo URL: " + profile.photoURL);
+          });
+        }
         return (
             <li className="nav-item ">
                 {
